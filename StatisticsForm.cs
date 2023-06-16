91e02995b2dad1133594dda59b8cafd674a63115
Michalis <57771939+MichelStath@users.noreply.github.com>
2023-06-16 20:57:54 +0300

    more statistic

--- StatisticsForm.cs
+++ StatisticsForm.cs
@@ -36,11 +36,11 @@ namespace LearningSoftware
             skillsLB.Text = allLessonViewList.Select(x => x.LESSON_4).Sum().ToString();
             totalLB.Text = allLessonViewList.Select(x => x.LESSON_1 + x.LESSON_2 + x.LESSON_3 + x.LESSON_4).Sum().ToString();
 
-            userIntroLB.Text = allLessonViewList.Where(x => x.S_ID == 1).Select(x => x.LESSON_1).First().ToString(); 
-            userLangLB.Text = allLessonViewList.Where(x => x.S_ID == 1).Select(x => x.LESSON_2).First().ToString(); 
-            userJobsLB.Text = allLessonViewList.Where(x => x.S_ID == 1).Select(x => x.LESSON_3).First().ToString(); 
-            userSKillsLB.Text = allLessonViewList.Where(x => x.S_ID == 1).Select(x => x.LESSON_4).First().ToString(); 
-            userTotalLB.Text = allLessonViewList.Where(x => x.S_ID == 1).Select(x => x.LESSON_1 + x.LESSON_2 + x.LESSON_3 + x.LESSON_4).Sum().ToString(); 
+            userIntroLB.Text = allLessonViewList.Where(x => x.S_ID == currentStudent.S_ID).Select(x => x.LESSON_1).First().ToString(); 
+            userLangLB.Text = allLessonViewList.Where(x => x.S_ID == currentStudent.S_ID).Select(x => x.LESSON_2).First().ToString(); 
+            userJobsLB.Text = allLessonViewList.Where(x => x.S_ID == currentStudent.S_ID).Select(x => x.LESSON_3).First().ToString(); 
+            userSKillsLB.Text = allLessonViewList.Where(x => x.S_ID == currentStudent.S_ID).Select(x => x.LESSON_4).First().ToString(); 
+            userTotalLB.Text = allLessonViewList.Where(x => x.S_ID == currentStudent.S_ID).Select(x => x.LESSON_1 + x.LESSON_2 + x.LESSON_3 + x.LESSON_4).Sum().ToString(); 
         
         
         }
