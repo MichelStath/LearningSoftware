fbb9598ea1449b4412fe43bbdd795638915e09be
Michalis <57771939+MichelStath@users.noreply.github.com>
2023-06-18 23:10:31 +0300

    JOB RESULTS READY
    TODO: ADD LINKS

--- JobResultsForm.Designer.cs
+++ JobResultsForm.Designer.cs
@@ -28,22 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 47);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "ΠΑΤΑ ΜΕ!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label2.Location = new System.Drawing.Point(12, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(486, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Πάτησε το κουμπί για να μάθεις περισσότερα";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(503, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Πάτησε το κουμπί για να δεις τα μεταπτυχιακά";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(17, 140);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 47);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "ΠΑΤΑ ΜΕ!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // JobResultsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "JobResultsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "JobResultsForm";
             this.Load += new System.EventHandler(this.JobResultsForm_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
     }
 }
--- JobResultsForm.cs
+++ JobResultsForm.cs
@@ -16,14 +16,14 @@ namespace LearningSoftware
         private Student currentStudent = new Student();
         private List<JobsClass> currentJobs = new List<JobsClass>();
         private JobsClass currentJob = new JobsClass();
+        private string link;
+        private string link2;
         public JobResultsForm(List<JobsClass> jobs, Student s)
         {
             InitializeComponent();
             currentStudent = s;
             currentJobs = jobs;
             currentJob = currentJobs.OrderByDescending(x => x.JOB_SCORE).First();
-
-
         }
 
         private void JobResultsForm_Load(object sender, EventArgs e)
@@ -35,23 +35,55 @@ namespace LearningSoftware
             {
                 case "SOFTWARE":
                     this.BackgroundImage = Properties.Resources.software;
+                    label1.Text = "η ΔΟΥΛΕΙΑ ΠΟΥ ΣΕ ΕΝΔΙΑΦΕΡΕΙ ΕΙΝΑΙ ΑΥΤΗ ΤΟΥ \nSoftware Developer";
+                    link = "";
+                    link2 = "";
                     break;
                 case "NETWORK":
                     this.BackgroundImage = Properties.Resources.network;
+                    label1.Text = "η ΔΟΥΛΕΙΑ ΠΟΥ ΣΕ ΕΝΔΙΑΦΕΡΕΙ ΕΙΝΑΙ ΑΥΤΗ ΤΟΥ \nNetwork Designer";
+                    link = "";
+                    link2 = "";
                     break;
                 case "UI":
                     this.BackgroundImage = Properties.Resources.UI;
+                    label1.Text = "η ΔΟΥΛΕΙΑ ΠΟΥ ΣΕ ΕΝΔΙΑΦΕΡΕΙ ΕΙΝΑΙ ΑΥΤΗ ΤΟΥ \nUX-UI dESIGNER";
+                    link = "";
+                    link2 = "";
                     break;
                 case "AI":
                     this.BackgroundImage = Properties.Resources.ai;
+                    label1.Text = "η ΔΟΥΛΕΙΑ ΠΟΥ ΣΕ ΕΝΔΙΑΦΕΡΕΙ ΕΙΝΑΙ ΑΥΤΗ ΤΟΥ \nAI Developer";
+                    link = "";
+                    link2 = "";
                     break;
                 case "TEACHER":
                     this.BackgroundImage = Properties.Resources.teacher;
+                    label1.Text = "η ΔΟΥΛΕΙΑ ΠΟΥ ΣΕ ΕΝΔΙΑΦΕΡΕΙ ΕΙΝΑΙ ΑΥΤΗ ΤΟΥ \nΚΑΘΗΓΗΤΗ ΠΛΗΡΟΦΟΡΙΚΗΣ";
+                    link = "";
+                    link2 = "";
                     break;
                 default:
+                    label1.Text = "";
+                    button1.Enabled=false;
+                    button2.Enabled = false;
+                    MessageBox.Show("Something gone wrong", "ERROR");
+                    this.Close();
                     break;
             }
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            WebBrowser f = new WebBrowser(link);
+            f.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            WebBrowser f = new WebBrowser(link2);
+            f.ShowDialog();
         }
     }
 }
